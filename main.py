6ee93f8f82786b5c90fcc324b279ad1be6eba37d
mgstabrani <mgstabrani19@gmail.com>
2021-05-10 00:12:39 +0700

    Add solution and output

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from data import *
-print(food)
-print(age_nutrition)
+from branchbound import *
+
+#Get solution
+solusi = findSolution(upperBound)
+
+#Get money used and calory
+uangUsed = 0
+kalori = 0
+for i in range(len(solusi)):
+    if(solusi[i] == 1):
+        uangUsed += food[i][2]
+        kalori += food[i][1]
+
+#Print output
+print("Total uang yang digunakan: Rp.",int(uangUsed))
+print("Total kalori yang didapatkan:",kalori, "kkal")
+print("Kalori per hari yang dibutuhkan:", age_nutrition[kategori-1][1], "kkal")
+print("Makanan yang didapatkan: ")
+j = 1
+for i in range(len(solusi)-1):
+    if(solusi[i] == 1):
+        print(str(j) + ". " + food[i][0])
+        j += 1
